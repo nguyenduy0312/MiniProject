a904fb8e74a058e84bc5469403a2b8392734afc1
nguyenduy0312 <160990102+nguyenduy0312@users.noreply.github.com>
2026-08-11 09:51:12 +0700

    Module 6: Add Employee list page

--- src/main/java/com/example/employeemanagement/controller/EmployeeViewController.java
+++ src/main/java/com/example/employeemanagement/controller/EmployeeViewController.java
@@ -1,13 +1,24 @@
 package com.example.employeemanagement.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.employeemanagement.service.EmployeeService;
 
 @Controller
 public class EmployeeViewController {
 
+    private final EmployeeService employeeService;
+
+    public EmployeeViewController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
     @GetMapping("/employees/list")
-    public String listEmployees() {
+    public String listEmployees(Model model) {
+        model.addAttribute("employees", employeeService.getAllEmployees());
+
         return "employees/list";
     }
 }
